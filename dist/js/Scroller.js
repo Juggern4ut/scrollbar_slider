8891eff494f87ce3f9f40ffb90700a5391dcdc4b
Lukas Meier <lukas.meier3@students.fhnw.ch>
2022-01-12 12:32:55 +0100

    Refactoring and commenting

--- dist/js/Scroller.js
+++ dist/js/Scroller.js
@@ -4,27 +4,51 @@ var Scroller = /** @class */ (function () {
         this.container = document.querySelector(selector);
         this.items = this.container.children;
     }
+    /**
+     * Will calculate the width of a given Element
+     * including horizontal margins
+     * @param slide The slide to calculate the width of
+     * @returns The width of the slide including margins
+     */
     Scroller.prototype.getTotalWidth = function (slide) {
         var styles = window.getComputedStyle(slide);
         return (parseInt(styles.marginRight) +
             parseInt(styles.marginLeft) +
             parseInt(styles.width));
     };
+    /**
+     * Scroll to the next page
+     */
     Scroller.prototype.gotoRight = function () {
-        var firstSlide = this.items[0];
-        var slideWidth = this.getTotalWidth(firstSlide);
-        var pageWidth = this.getSlideAmount() * slideWidth;
-        var targetPage = Math.ceil((this.container.scrollLeft + 1) / pageWidth);
-        this.container.scroll(targetPage * pageWidth, 0);
+        this.goto(false);
     };
+    /**
+     * Scroll to the previous page
+     */
     Scroller.prototype.gotoLeft = function () {
+        this.goto(true);
+    };
+    /**
+     * Will scroll the slider either right or left
+     * to the next page, depending on the given parameter
+     * @param left If set to true, the slider will scroll left, and right otherwise
+     */
+    Scroller.prototype.goto = function (left) {
+        if (left === void 0) { left = false; }
         var firstSlide = this.items[0];
         var slideWidth = this.getTotalWidth(firstSlide);
-        var pageWidth = this.getSlideAmount() * slideWidth;
-        var targetPage = Math.floor((this.container.scrollLeft - 1) / pageWidth);
+        var pageWidth = this.getElementsPerPage() * slideWidth;
+        var targetPage = left
+            ? Math.floor((this.container.scrollLeft - 1) / pageWidth)
+            : Math.ceil((this.container.scrollLeft + 1) / pageWidth);
         this.container.scroll(targetPage * pageWidth, 0);
     };
-    Scroller.prototype.getSlideAmount = function () {
+    /**
+     * Will return the number of Elements that can
+     * be simoutaniusly displayed per Page
+     * @returns The number of elements per Page
+     */
+    Scroller.prototype.getElementsPerPage = function () {
         var slide = this.items[0];
         var slideWidth = this.getTotalWidth(slide);
         var containerWidth = parseInt(window.getComputedStyle(this.container).width);
--- src/ts/Scroller.ts
+++ src/ts/Scroller.ts
@@ -7,6 +7,12 @@ class Scroller {
     this.items = this.container.children;
   }
 
+  /**
+   * Will calculate the width of a given Element
+   * including horizontal margins
+   * @param slide The slide to calculate the width of
+   * @returns The width of the slide including margins
+   */
   getTotalWidth(slide: HTMLElement): number {
     const styles = window.getComputedStyle(slide);
     return (
@@ -16,23 +22,41 @@ class Scroller {
     );
   }
 
-  gotoRight() {
+  /**
+   * Scroll to the next page
+   */
+  gotoRight(): void {
+    this.goto(false);
+  }
+
+  /**
+   * Scroll to the previous page
+   */
+  gotoLeft(): void {
+    this.goto(true);
+  }
+
+  /**
+   * Will scroll the slider either right or left
+   * to the next page, depending on the given parameter
+   * @param left If set to true, the slider will scroll left, and right otherwise
+   */
+  goto(left: boolean = false) {
     const firstSlide = this.items[0] as HTMLElement;
     const slideWidth = this.getTotalWidth(firstSlide);
-    const pageWidth = this.getSlideAmount() * slideWidth;
-    const targetPage = Math.ceil((this.container.scrollLeft + 1) / pageWidth);
+    const pageWidth = this.getElementsPerPage() * slideWidth;
+    const targetPage = left
+      ? Math.floor((this.container.scrollLeft - 1) / pageWidth)
+      : Math.ceil((this.container.scrollLeft + 1) / pageWidth);
     this.container.scroll(targetPage * pageWidth, 0);
   }
 
-  gotoLeft() {
-    const firstSlide = this.items[0] as HTMLElement;
-    const slideWidth = this.getTotalWidth(firstSlide);
-    const pageWidth = this.getSlideAmount() * slideWidth;
-    const targetPage = Math.floor((this.container.scrollLeft - 1) / pageWidth);
-    this.container.scroll(targetPage * pageWidth, 0);
-  }
-
-  getSlideAmount(): number {
+  /**
+   * Will return the number of Elements that can
+   * be simoutaniusly displayed per Page
+   * @returns The number of elements per Page
+   */
+  getElementsPerPage(): number {
     const slide = this.items[0] as HTMLElement;
     const slideWidth = this.getTotalWidth(slide);
     const containerWidth = parseInt(
